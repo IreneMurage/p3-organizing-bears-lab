573042ba7942b31b7ff105eb6ad3efaf4b2376af
IreneMurage <muthonimurage361@gmail.com>
2025-08-16 14:01:35 +0300

    Add SQL table and initial data for bears; update queries for selection

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperement, alive) VALUES
+('Mr. Chocolate', 20, 'M', 'brown', 'calm', 1),
+('Rowdy', 15, 'M', 'black', 'aggressive', 1),
+('Tabitha', 8, 'F', 'white', 'friendly', 1),
+('Sergeant Brown', 12, 'M', 'brown', 'serious', 1),
+('Melissa', 9, 'F', 'black', 'playful', 1),
+('Grinch', 17, 'M', 'green', 'grumpy', 0),
+('Wendy', 6, 'F', 'gray', 'sweet', 1),
+(NULL, 4, 'M', 'brown', 'curious', 1);
